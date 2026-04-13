0732bc63f337e43c249b07ac330e09693e001010
Andreas Möller <am@localheinz.com>
2026-04-13 17:14:53 +0200

    Enhancement: Enable RemoveNamedArgumentForSingleParameterRector

--- rector.php
+++ rector.php
@@ -31,8 +31,8 @@ return static function (Config\RectorConfig $rectorConfig): void {
 
     $rectorConfig->rules([
         Php81\Rector\Property\ReadOnlyPropertyRector::class,
-        Rector\Rules\Expressions\CallLikes\RemoveNamedArgumentForSingleParameterRector::class,
         Rector\Rules\Expressions\Arrays\SortAssociativeArrayByKeyRector::class,
+        Rector\Rules\Expressions\CallLikes\RemoveNamedArgumentForSingleParameterRector::class,
         Rector\Rules\Expressions\Matches\SortMatchArmsByConditionalRector::class,
         Rector\Rules\Faker\GeneratorPropertyFetchToMethodCallRector::class,
         Rector\Rules\PHPUnit\ReplaceTestAttributeWithTestPrefixRector::class,
